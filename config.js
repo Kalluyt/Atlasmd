87ed2b256a2a435a4c1fc7268df79082a944c465
Kalluyt <109035110+Kalluyt@users.noreply.github.com>
2023-04-11 19:03:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -32,20 +32,20 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "12036646449";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
 
 
-global.owner = gg.split(",");
+global.owner = gg.split("12036646449");
 global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `TANMAY TATE`;
+global.author = process.env.AUTHOR || "by: tanmay";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
@@ -55,19 +55,19 @@ module.exports = {
 // ---------------------Do Not Modify this part------------------- //
 
 global.mess = {
-  jobdone: "Job done...",
-  useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
+  jobdone: "Hogaya,Done...",
+  useradmin: "Be bas group admin use karpayenge*lauda*,Sorry, only *Group Admins* can use this command *Baka*!",
   botadmin:
-    "Sorry, i cant execute this command without being an *Admin* of this group.",
-  botowner: "Only my *Owner* can use this command, Baka!",
-  grouponly: "This command is only made for *Groups*, Baka!",
-  privateonly: "This command is only made for *Private Chat*, Baka!",
+    "Sorry, Admin de laude i cant execute this command without being an *Admin* of this group.",
+  botowner: "Bas tanmay k liye hai, Baka!",
+  grouponly: "Group me kaam karega, Baka!",
+  privateonly: "This command is only made for *Direct message*, Baka!",
   botonly: "Only the *Bot itself* can use this command!",
-  waiting: "Chotto Matte...",
-  nolink: "Please provide me *link*, Baka!",
-  error: "An error occurd!",
-  banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
-  bangc: "This Group is *Banned* from using Commands!",
-  nonsfw: "Dont be a pervert Baka! This is not a NSFW enabled group!",
+  waiting: "Ruk ja laude...",
+  nolink: " *link*de , Baka!",
+  error: "Bhosdike error agaya!",
+  banned: `Tu *Banned* hai using commands!  \n\nType *${prefa}owner* ask for unban !`,
+  bangc: "Ye group *Banned* hai from using Commands!",
+  nonsfw: "Bhosdike simp mat ban Baka! This is not a NSFW enabled group!",
 };
 
